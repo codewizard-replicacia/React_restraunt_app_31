feddb904b028dc26435aa7ceb26cdc28202427a1
Administrator <Administrator@172.31.36.201>
2023-09-14 05:07:08 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,41 @@
+export const productViewConfig = {
+Details: [
+    {
+      key: "ProductId",
+      value: "ProductId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ProductName",
+      value: "ProductName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ProductDescription",
+      value: "ProductDescription",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ProductImage",
+      value: "ProductImage",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ProductType",
+      value: "ProductType",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
